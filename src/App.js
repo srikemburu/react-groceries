7afb1b991a72b961639f4af5f8036f8c5d05db90
Anu and Sri Kemburu <avisfy0@hotmail.com>
2022-03-27 21:11:09 -0400

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,102 @@
-import logo from './logo.svg';
-import './App.css';
+
+import React, { useState } from "react";
+import "./App.css";
+import Item from "./components/Item";
+import { v4 as uuidv4 } from "uuid";
+
+const arr = () => {
+  let data = localStorage.getItem("data");
+  if (data) return JSON.parse(localStorage.getItem("data"));
+  else return [];
+};
 
 function App() {
+   const [item, setItem] = useState("");
+
+  console.log("start iem: " + item)
+  const [edit, setEdit] = useState(false);
+  const [editId, setEditId] = useState();
+  const [list, setList] = useState(arr);
+  const [error, setError] = useState("");
+
+  const handleSubmit = (e) => {
+    const newItem = {
+      id: uuidv4(),
+      item: item,
+      complete: false,
+    };
+    e.preventDefault();
+    if (item && item.length <= 25 && !edit) {
+      setList([...list, newItem]);
+      setItem("");
+      setError("");
+    } else if (item && item.length <= 25 && edit && editId) {
+      setList(
+        list.map((el) => {
+          if (el.id === editId) {
+            return { ...el, item: item };
+          }
+          return el;
+        })
+      );
+      setItem("");
+      setEditId(null);
+      setEdit(false);
+      setError("");
+    } else if (!item) setError("Item cannot be blank.");
+    else if (item.length > 25) setError("Character limit is 25.");
+  };
+
+  React.useEffect(() => {
+    localStorage.setItem("data", JSON.stringify(list));
+
+    console.log(list)
+  }, [list]);
+
+  const handleChange = (e) => {
+    setItem(e.target.value);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Grocery List</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          className="input"
+          type="text"
+          value={item}
+          placeholder="Item,Brand,Units,Quantity"
+          onChange={handleChange}
+        />
+        {edit ? (
+          <button className="btn" type="submit">
+            Edit Item
+          </button>
+        ) : (
+          <button className="btn" type="submit">
+            Add Item
+          </button>
+        )}
+        {error && <p style={{ color: "red" }}>{error}</p>}
+      </form>
+      <div>
+        {list.map((c, id) => (
+          <>
+          <Item
+            key={id}
+            id={c.id}
+            item={c.item}
+            list={list}
+            setList={setList}
+            complete={c.complete}
+            setItem={setItem}
+            setEdit={setEdit}
+            setEditId={setEditId}
+          />
+          </>
+        )
+        )}
+      </div>
     </div>
   );
 }
